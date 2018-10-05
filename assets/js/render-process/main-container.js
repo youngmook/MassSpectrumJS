d3f7fa2448ea2adbb50cb71f2cf13d38325b20b7
Young-Mook Kang, PhD <ymkang@thylove.org>
2018-10-05 11:00:48 +0900

    + Add main-container.js

---
+++ assets/js/render-process/main-container.js
@@ -0,0 +1,16 @@
+/**
+ * Mass Spectrum JS, a Mass Spectrum Viewer
+ * @author Young-Mook Kang <ymkang@thylove.org>
+ * @copyright 2018 Young-Mook Kang <ymkang@thylove.org>
+ * @license http://www.opensource.org/licenses/ISC The ISC License
+ * @repository https://github.com/youngmook/MassSpectrumJS
+ */
+
+const theMainContainer = {
+
+};
+
+
+webix.ready(function () {
+    webix.ui(theMainContainer);
+});
