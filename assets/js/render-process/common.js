38eba69894cf5a990e7ef679f40521a5aca65037
Young-Mook Kang, PhD <ymkang@thylove.org>
2018-10-05 11:01:42 +0900

    + Add index.html

--- assets/js/render-process/common.js
+++ assets/js/render-process/common.js
@@ -1,3 +1,11 @@
+/**
+ * Mass Spectrum JS, a Mass Spectrum Viewer
+ * @author Young-Mook Kang <ymkang@thylove.org>
+ * @copyright 2018 Young-Mook Kang <ymkang@thylove.org>
+ * @license http://www.opensource.org/licenses/ISC The ISC License
+ * @repository https://github.com/youngmook/MassSpectrumJS
+ */
+
 if(isDesktopEnvironment()) {
     window.jQuery = require('jquery');
 } else {
